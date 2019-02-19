1eae8304b1bc7087acb0d5253ca02c24ff954cd4
LetMe666 <201608030215@zut.edu.cn>
2019-02-19 22:17:27 +0800

    create user and userdao ,usermapp

---
+++ src/main/java/zut/cs/network/bookshop/dao/UserDao.java
@@ -0,0 +1,23 @@
+package zut.cs.network.bookshop.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import zut.cs.network.bookshop.entity.User;
+@Mapper
+public interface UserDao {
+	// 添加用户
+	void addUser(User user);
+
+	/*// 根据激活码查找用户
+	public User findUserByActiveCode(String activeCode);
+
+	// 激活用戶
+	public void activeUser(String activeCode);
+
+	// 根据用户名与密码查找用户
+	public User findUserByUsernameAndPassword(String username, String password);*/
+	//列出所有用户
+	List<User> AllUser();
+}
---
+++ src/test/java/zut/cs/network/bookshop/dao/UserDaoTest.java
@@ -0,0 +1,47 @@
+package zut.cs.network.bookshop.dao;
+
+
+
+import java.util.List;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import zut.cs.network.bookshop.entity.User;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserDaoTest {
+
+	
+    @Autowired
+    UserDao userDao;
+	@Test
+
+	public void testAddUser() {
+		User user=new User();
+		user.setUsername("test");
+		user.setEmail("1845508414@qq.com");
+		user.setIntroduce("学生");
+		user.setActiveCode("1234");
+		user.setGender("男");
+		user.setPassword("test");
+		user.setTelephone("123456789");
+		System.out.println(user.toString());
+		userDao.addUser(user);	
+	}
+	@Test
+	
+	public void testAllUser() {
+		List<User> users;	
+		users=userDao.AllUser();
+		for(int i=0;i<users.size();i++)
+		{
+		System.out.println(users.get(i).toString());
+		}
+	}
+
+}
